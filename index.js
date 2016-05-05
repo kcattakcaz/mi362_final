6828522e0dd95b53910f4aa4afc520bcadcd03db
Zachary Jaghory <zj@zach-jaghory.com>
2016-05-04 21:35:18 -0400

    Register

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 var express = require('express');
+var pg = require('pg');
 var app = express();
 
 app.set('port', (process.env.PORT || 5000));
@@ -11,6 +12,22 @@ app.set('view engine', 'ejs');
 
 app.get('/', function(request, response) {
   response.render('pages/index');
+});
+
+app.get('/register',function(request, response){
+  var name = request.param('name');
+  var email = request.param('email');
+  var pass = request.param('pass');
+  pg.connect(process.env.DATABASE_URL, function(err, client, done) {
+    client.query('INSERT INTO users (name,username,password) values ('+name+','+email+','+pass+')', function(err, result) {
+      done();
+      if (err)
+      { console.error(err); response.json(false); }
+      else
+      {   response.json(true); }
+    });
+  });
+
 });
 
 app.listen(app.get('port'), function() {
